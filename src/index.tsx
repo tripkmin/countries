504e777b85078c9e02e8a49dd245cf0670a37e5d
tripkmin <tripkmin@gmail.com>
2023-12-06 03:36:17 +0900

    fix: Resolve scroll not initializing issue

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from 'hooks/useScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
