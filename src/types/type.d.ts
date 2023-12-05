d8f03f5603b7bf319aab9e5172b36f9417c2f4c2
tripkmin <tripkmin@gmail.com>
2023-12-05 10:24:02 +0900

    chore: Update data.json and types

--- src/types/type.d.ts
+++ src/types/type.d.ts
@@ -1,72 +1,122 @@
 import { ReactNode } from 'react';
 
+// Nation Type 관련 시작
 export interface NationT {
-  name: string;
-  topLevelDomain: string[];
-  alpha2Code: string;
-  alpha3Code: string;
-  callingCodes: string[];
-  capital?: string;
-  altSpellings?: string[];
-  subregion: string;
+  name: Name;
+  tld?: string[];
+  cca2: string;
+  ccn3?: string;
+  cca3: string;
+  independent?: boolean;
+  status: string;
+  unMember: boolean;
+  currencies?: Currencies;
+  idd: Idd;
+  capital?: string[];
+  altSpellings: string[];
   region: string;
-  population: number;
-  latlng?: number[];
-  demonym: string;
-  area?: number;
-  timezones: string[];
-  borders?: string[];
-  nativeName: string;
-  numericCode: string;
-  flags: Flags;
-  currencies?: Currency[];
-  languages: Language[];
+  subregion?: string;
+  languages?: Languages;
   translations: Translations;
+  latlng: number[];
+  landlocked: boolean;
+  area: number;
+  demonyms?: Demonyms;
   flag: string;
-  regionalBlocs?: RegionalBloc[];
+  maps: Maps;
+  population: number;
+  car: Car;
+  timezones: string[];
+  continents: string | string[];
+  flags: Flags;
+  coatOfArms: CoatOfArms;
+  startOfWeek: string;
+  capitalInfo: CapitalInfo;
+  postalCode?: PostalCode;
   cioc?: string;
-  independent: boolean;
-  gini?: number;
-}
-
-export interface Currency {
-  code: string;
-  name: string;
-  symbol: string;
-}
-
-export interface Flags {
-  svg: string;
-  png: string;
-}
-
-export interface Language {
-  iso639_1?: string;
-  iso639_2: string;
-  name: string;
-  nativeName?: string;
-}
-
-export interface RegionalBloc {
-  acronym: string;
-  name: string;
-  otherNames?: string[];
-  otherAcronyms?: string[];
+  borders?: string[];
+  fifa?: string;
+  gini?: { [key: string]: number | undefined };
 }
 
 export interface Translations {
-  br: string;
-  pt: string;
-  nl: string;
-  hr: string;
-  fa?: string;
-  de: string;
-  es: string;
-  fr: string;
-  ja: string;
-  it: string;
-  hu: string;
+  [key: string]: Translation | undefined;
 }
+
+export interface Languages {
+  [key: string]: string | undefined;
+}
+
+export interface Currencies {
+  [key: string]: Aed | undefined;
+}
+
+export interface Aed {
+  name: string;
+  symbol?: string;
+}
+
+export interface CapitalInfo {
+  latlng?: number[];
+}
+
+export interface Car {
+  signs?: string[];
+  side: string;
+}
+
+export interface CoatOfArms {
+  png?: string;
+  svg?: string;
+}
+
+export interface BAM {
+  name: string;
+}
+
+export interface Demonyms {
+  eng: Eng;
+  fra?: Eng;
+}
+
+export interface Eng {
+  f: string;
+  m: string;
+}
+
+export interface Flags {
+  png: string;
+  svg: string;
+  alt?: string;
+}
+
+export interface Idd {
+  root?: string;
+  suffixes?: string[];
+}
+
+export interface Maps {
+  googleMaps: string;
+  openStreetMaps: string;
+}
+
+export interface Name {
+  common: string;
+  official: string;
+  nativeName?: { [key: string]: Translation | undefined };
+}
+
+export interface Translation {
+  official: string;
+  common: string;
+}
+
+export interface PostalCode {
+  format: string;
+  regex?: string;
+}
+
+// Nation Type 관련 끝
 
 export interface LayoutProps {
   children?: ReactNode;
